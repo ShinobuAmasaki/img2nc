1fe3b5ed4440ad42d2d191edd16cbd7ca78cd108
Shinobu Amasaki <amasaki203@gmail.com>
2022-09-04 14:32:52 +0900

    [update] trial_mpi: comment-outed text cleaned.

--- app/trial_mpi.f90
+++ app/trial_mpi.f90
@@ -42,6 +42,7 @@ program trial_mpi
    outnc = '/home/shin0/WORK/img2nc/out_04-13.nc'
    range = '0/4/-1/3'
    priority = 'lon'
+
    call edge%set_four(0, 4, -1, 3)
 
    call create_name_list(data_dir, edge, name_list)
@@ -58,11 +59,6 @@ program trial_mpi
    if (this == 1) then
       print *, 'start: loading img files'
    end if
-
-   
-   ! do j = 1, local%nlat_img
-   !    do i = local%i_begin, local%i_end
-       !-*- DO NOT exchange i,j order for valid processing of function total_size_of_tile_array -*-
 
    do j = local%lat_begin, local%lat_end
       do i = local%lon_begin, local%lon_end
@@ -84,36 +80,20 @@ program trial_mpi
 
 !----
    ! merge tiles on each rank
-   ! print *, this, ':', local%i_begin, local%i_end : PASS
-
    call merge_tiles(array(local%lon_begin:local%lon_end, local%lat_begin:local%lat_end), single)
    call mpi_barrier(mpi_comm_world, ierr)
 
-   ! print *, single%west_lon, single%east_lon, single%south_lat, single%north_lat
-   ! print *, size(single%data, dim=1), size(single%data, dim=2)
-   ! call on_test_finalize()
-
 !----
    call local%set_nlon_nlat(single)
-      ! local%nlon = size(single%data, dim=1)
-      ! local%nlat = size(single%data, dim=2)
 
    allocate(global%local_nlon(petot), source=0)
    allocate(global%local_nlat(petot), source=0)
 
    ! globalに各localのnlon,nlatをallgatherする。
    call global%gather_local_nlon_nlat(local, mpi_comm_world, ierr)
-      ! global%local_nlon(1) = local%nlon
-      ! global%local_nlat(1) = local%nlat
-      ! call mpi_allgather(global%local_nlon(1), 1, mpi_integer4, global%local_nlon(1), 1, mpi_integer4, mpi_comm_world, ierr)
-      ! call mpi_allgather(global%local_nlat(1), 1, mpi_integer4, global%local_nlat(1), 1, mpi_integer4, mpi_comm_world, ierr)
    call global%set_nlon_nlat(priority=priority)
-      ! global%nlon = sum(global%local_nlon(:), dim=1)
-      ! global%nlat = local%nlat   !緯度方向優先で経度を分割しているため
-
 
    call mpi_barrier(mpi_comm_world, ierr)
-   ! print *, this, ':', local%i_e_begin, local%i_e_end
 
 !---------------------!
 !  Single processing  !
@@ -130,49 +110,27 @@ program trial_mpi
 
 !-----------------------!
 !  Multiple processing  !
-   
-
    if (this == 1) then
+      ! recieving data array on master
       allocate(data_array(global%nlon, global%nlat), source=int2(0))
-      print *, 'global:', global%nlon*global%nlat, global%nlon, global%nlat
    end if
-   call mpi_barrier(mpi_comm_world, ierr)
-
    n_send = local%nlon*local%nlat
-   print *, this, ':', n_send, local%nlon, local%nlat
    call mpi_barrier(mpi_comm_world, ierr)
-
-   ! -- MPI Comm derived type definition
-   ! call mpi_type_vector(local%nlon, local%nlat, local%nlat, mpi_integer2, t_integrate, ierr) ! FAILED
-   ! call mpi_type_subarray()
-   ! call mpi_type_commit(t_integrate, ierr)
-   ! --
-
-
-
-
 
    if (this == 1) then
       print *, 'gather: start gather.'
    end if
-   ! ERROR test_22: PASS
+
+   ! -*- PASS: test_22, n=4 -*-
    call mpi_gather(single%data(1,1), n_send, mpi_integer2, data_array(1,1), n_send, mpi_integer2, 0, mpi_comm_world, ierr)
    ! call mpi_gather(single%data(1,1), 1, t_integrate, data_array(1,1), 1, t_integrate, 0, mpi_comm_world, ierr)
  
-   call mpi_barrier(mpi_comm_world, ierr)
    if (this == 1) then
       print *, 'gather: end gather.'
    end if
 
-   ! if (allocated(single%data)) then
-   !    deallocate(single%data)
-   ! end if
 
    if (this == 1) then
-      ! call edge%set_four(0, 1, -1, 3)
-      ! call single%read_boundary(edge)
-      ! call nc_output(outnc, single)
-
       print *, 'multiple-process: nc making.'
       final_tile%p_data => data_array
       call final_tile%read_boundary(edge)
